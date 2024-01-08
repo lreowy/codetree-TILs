3e81fcb804f02c6a2b5278fff9a63f50dd8c432d
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-01-08 17:16:00 +0900

    Title: 4번의 크기 비교, Time: 148ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240108/4번의 크기 비교/4-time-comparison.py
@@ -0,0 +1,25 @@
+a = int(input())
+s = input().split()
+b = int(s[0])
+c = int(s[1])
+d = int(s[2])
+e = int(s[3])
+
+if a>b:
+    print(1)
+else:
+    print(0)
+
+if a>c:
+    print(1)
+else:
+    print(0)
+
+if a>d:
+    print(1)
+else:
+    print(0)
+if a>e:
+    print(1)
+else:
+    print(0)
