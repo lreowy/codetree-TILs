dee0d3a735191d9b546a7095becab3228c8a7176
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-01-09 22:26:49 +0900

    Title: 공약수의 유무, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240109/공약수의 유무/presence-or-absence-of-a-common-divisor.py
+++ 240109/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -1,14 +1,13 @@
 s = input().split()
 a = int(s[0])
 b = int(s[1])
-i = 1
-while True:
-    if i>=2:
-        if a%i ==0 and b%i ==0:
-            i += 1
+i = 2
+while True: 
+    if a%i ==0 and b%i ==0:
+        i += 1
     else:
         break
-if i == 1:
+if i == 2:
     print(0)
 else:
     print(1)
