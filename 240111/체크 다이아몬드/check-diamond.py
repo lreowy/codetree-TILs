08078c83ba46f957f68a7110317ec803e11db959
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-01-11 20:41:52 +0900

    Title: 체크 다이아몬드, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240111/체크 다이아몬드/check-diamond.py
+++ 240111/체크 다이아몬드/check-diamond.py
@@ -1,13 +1,13 @@
 n = int(input())
 
 for i in range(1,n+1):
-    for _ in range(n-i-1,-1,-1):
+    for _ in range(n-i,0,-1):
         print(" ",end="")
     for _ in range(i):
         print("*",end=" ")
     print("")
 for i in range(n-1,0,-1):
-    for _ in range(n-i-1,-1,-1):
+    for _ in range(n-i,0,-1):
         print(" ",end="")
     for _ in range(i):
         print("*",end=" ")
