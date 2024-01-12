0414fa05874d74e11fbce2706e07bf2bbb349773
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-01-12 23:12:06 +0900

    Title: 나눗셈의 나머지, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240112/나눗셈의 나머지/remainder-of-division.py
@@ -0,0 +1,20 @@
+s = input().split()
+a = int(s[0])
+b = int(s[1])
+ans = a
+arr = []
+cnt = 0
+ssum =0
+while True:
+    ans = ans//b
+    arr.append(ans%b)
+    if ans <= 1:
+        break
+count = [0] * 10
+
+for elem in arr:
+    count[elem] += 1
+
+for i in range(len(count)):
+    ssum += count[i]**2
+print(ssum)
