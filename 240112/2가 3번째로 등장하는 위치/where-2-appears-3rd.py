aa66355e66dd905cf6cadba0c9c4b95494249662
Yerin Lee <91695537+lxxyxin@users.noreply.github.com>
2024-01-12 23:34:10 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240112/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
+++ 240112/2가 3번째로 등장하는 위치/where-2-appears-3rd.py
@@ -2,9 +2,9 @@ n = int(input())
 arr = list(map(int,input().split()))
 count = 0
 idx = 0
-for i in range(len(arr)):
+for i in range(n):
+    idx += 1
     if arr[i] == 2:
-        idx += 1
         count += 1
     if count == 3:
         print(idx)
