1f0583649b25e690d0a0de1cdea30381685149b2
Yerin Lee <91695537+lreowy@users.noreply.github.com>
2024-09-04 17:11:15 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 81ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240904/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void printStars(){
+        System.out.println("**********");
+    }
+    public static void main(String[] args) {
+        for (int i=0;i<5;i++){
+            printStars();
+        }
+    }
+}
